60536825ef7f936c1b8e64ff8d2c8dc1af5324d5
John Rowley <outsidethebox@eircom.net>
2017-10-10 13:31:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo "<h1>Hello World Version 0.2</h1>";
 echo "<p>Hello World from Heroku</p>";
 echo "<p>This is a new paragraph</p>";
-echo "<p><a href="peopleapi.php">Link to People API</a></p>";
+echo "<p><a href='peopleapi.php'>Link to People API</a></p>";
 
